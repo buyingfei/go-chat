c5d29e5b2f666ab27eb01c1bf155a22ee35b7939
buyingfei <buyf@2345/com>
2018-06-19 16:25:46 +0800

    允许跨域访问

--- backend/controllers/websocket.go
+++ backend/controllers/websocket.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/logs"
 	"github.com/gorilla/websocket"
 	"net/http"
 )
@@ -12,18 +11,22 @@ type WebsocketController struct {
 	beego.Controller
 }
 
+var upgrader = websocket.Upgrader{
+} // use default options
 
 // Join method handles WebSocket requests for WebSocketController.
 func (this *WebsocketController) Get() {
-
-	ws, err := websocket.Upgrade(this.Ctx.ResponseWriter, this.Ctx.Request, nil, 1024, 1024)
-	if _, ok := err.(websocket.HandshakeError); ok {
-		http.Error(this.Ctx.ResponseWriter, "Not a websocket handshake", 400)
-		return
-	} else if err != nil {
+	// 允许跨域
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	// // 建立socket 链接
+	ws, err := upgrader.Upgrade(this.Ctx.ResponseWriter, this.Ctx.Request, nil)
+	if err != nil {
 		beego.Error("Cannot setup WebSocket connection:", err)
 		return
 	}
+	defer ws.Close()
 	for {
 		mt, message, err := ws.ReadMessage()
 		if err != nil {
@@ -32,7 +35,7 @@ func (this *WebsocketController) Get() {
 		}
 		err = ws.WriteMessage(mt, message)
 		if err != nil {
-			logs.Info("write:", err)
+			beego.Info("write:", err)
 			break
 		}
 	}
